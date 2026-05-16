492a775707d45c825e8e78b5366207961a4671e4
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-16 11:16:07 -0500

    feat(incidencia): importar la entidad en el modulo usando typeorm

--- src/incidencia/incidencia.module.ts
+++ src/incidencia/incidencia.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { IncidenciaService } from './incidencia.service';
 import { IncidenciaController } from './incidencia.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Incidencia } from './entities/incidencia.entity';
 
 @Module({
   controllers: [IncidenciaController],
   providers: [IncidenciaService],
+  imports: [
+    TypeOrmModule.forFeature([ Incidencia ])
+  ]
 })
 export class IncidenciaModule {}
