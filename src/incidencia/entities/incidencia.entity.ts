23cfdd2cc4cb64be475f3a1f1339ea90350c9f25
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-16 11:09:59 -0500

    feat(incidencia): crear entity de incidencia

--- src/incidencia/entities/incidencia.entity.ts
+++ src/incidencia/entities/incidencia.entity.ts
@@ -1,1 +1,20 @@
-export class Incidencia {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Incidencia {
+
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
+
+    @Column({ type: 'date' })
+    fecha: Date;
+
+    @Column({ type: 'enum', enum: ['baja', 'media', 'alta'], default: 'baja'})
+    gravedad: string;
+
+    @Column({ type: 'text', nullable: true})
+    descripcion: string;
+    
+    @Column({type: 'enum', enum: ['pendiente', 'en_proceso', 'resuelto'], default: 'pendiente'})
+    estado: string;
+}
