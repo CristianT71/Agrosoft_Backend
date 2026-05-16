2dac492156fc3d38e4205de6776cf381115810d3
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-16 11:44:24 -0500

    feat(cosecha): crear entity de cosecha

--- src/cosecha/entities/cosecha.entity.ts
+++ src/cosecha/entities/cosecha.entity.ts
@@ -1,1 +1,23 @@
-export class Cosecha {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Cosecha {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ type: 'date' })
+    fecha_cosecha: Date;
+
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    cantidad_cosechada: number;
+
+    @Column({ type: 'varchar', length: 50 })
+    unidad_medida: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    tipo_cosecha: string;
+
+    @Column({ type: 'text', nullable: true })
+    observaciones: string;
+}
