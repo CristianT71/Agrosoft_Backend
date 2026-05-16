b90e3e0b4da841fc7402808615c62abf826d5487
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-16 10:35:26 -0500

    feat(usuario): crear entity de usuario

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,1 +1,23 @@
-export class Usuario {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Usuario {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({type: 'varchar', length: 150})
+    nombre: string;
+
+    @Column({type: 'varchar', length: 150, unique: true})
+    correo: string;
+
+    @Column({type: 'varchar', length: 255, select: false})
+    contraseña: string;
+
+    @Column({type: 'enum', enum: ['activo', 'inactivo', 'suspendido'], default: 'activo'})
+    estado: string;
+    
+    @Column({type: 'varchar', length: 20, nullable: true})
+    telefono: string;
+}
