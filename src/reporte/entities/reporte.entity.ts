d8d36159728a1a7403208ee8647d7bfbce47b648
Juan Valencia <juandavidpv2006@gmail.com>
2026-05-20 20:44:41 -0500

    estoy empezando a configurar entity y agregando columnas

--- src/reporte/entities/reporte.entity.ts
+++ src/reporte/entities/reporte.entity.ts
@@ -1,1 +1,15 @@
-export class Reporte {}
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class Reporte {
+    id:string;
+
+    @Column()
+    tipo_reporte: string;
+
+    @Column()
+    fecha_creacion: string;
+
+    @Column()
+    formato_reporte: string;
+}
