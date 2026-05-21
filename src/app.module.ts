149836b3dd14472ae04c1daa6018260d9d9a82c0
Juan Valencia <juandavidpv2006@gmail.com>
2026-05-20 19:19:32 -0500

    acabo de crear el modulo de Insumo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { InsumoModule } from './insumo/insumo.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    InsumoModule,
   ],
   controllers: [],
   providers: [],
