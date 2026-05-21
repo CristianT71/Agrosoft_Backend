2c3cac4b0018cfa1ec7aa2a6d8fab3f6648c924c
Juan Valencia <juandavidpv2006@gmail.com>
2026-05-20 21:29:00 -0500

    estoy en el create de vente haciendo cambios

--- src/venta/dto/create-venta.dto.ts
+++ src/venta/dto/create-venta.dto.ts
@@ -1,1 +1,27 @@
-export class CreateVentaDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+
+export class CreateVentaDto {
+    @IsString()
+    @IsNotEmpty()
+    fecha_venta: string;
+
+    @IsString()
+    @IsNotEmpty()
+    cantidad_vendida: string;
+
+    @IsString()
+    @IsNotEmpty()
+    precio_unitario: string;
+
+    @IsString()
+    @IsNotEmpty()
+    ingreso_total: string;
+
+    @IsString()
+    @IsNotEmpty()
+
+    @IsString()
+    @IsNotEmpty()
+
+}
