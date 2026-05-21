5ceeb0ef2b0458e92284d17c08c24a5a6911c33a
Juan Valencia <juandavidpv2006@gmail.com>
2026-05-20 21:25:29 -0500

    acabo de configurar el entity.ts de venta

--- src/venta/entities/venta.entity.ts
+++ src/venta/entities/venta.entity.ts
@@ -1,1 +1,25 @@
-export class Venta {}
+import { IsString } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Venta {
+    @PrimaryGeneratedColumn('uuid')
+
+    @Column()
+    fecha_venta: string;
+
+    @Column()
+    cantidad_vendida: string;
+
+    @Column()
+    precio_unitario: string;
+
+    @Column()
+    ingreso_total: string;
+
+    @Column()
+    forma_pago: string;
+
+    @Column()
+    estado_pago: string;
+}
