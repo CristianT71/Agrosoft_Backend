63af915d4a0cc8fe53d513745ecf8f242c06f771
Juan Valencia <juandavidpv2006@gmail.com>
2026-05-20 21:07:02 -0500

    acabo de configurar todo el service.ts

--- src/reporte/dto/create-reporte.dto.ts
+++ src/reporte/dto/create-reporte.dto.ts
@@ -1,1 +1,17 @@
-export class CreateReporteDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+
+export class CreateReporteDto {
+    @IsString()
+    @IsNotEmpty()
+    tipo_reporte: string;
+
+    @IsString()
+    @IsNotEmpty()
+    fecha_creacion: string;
+
+    @IsString()
+    @IsNotEmpty()
+    formato_reporte: string;
+}
+
--- src/reporte/reporte.service.ts
+++ src/reporte/reporte.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateReporteDto } from './dto/create-reporte.dto';
 import { UpdateReporteDto } from './dto/update-reporte.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Reporte } from './entities/reporte.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ReporteService {
-  create(createReporteDto: CreateReporteDto) {
-    return 'This action adds a new reporte';
+
+  constructor(
+    @InjectRepository (Reporte)
+    private readonly ReporteRepository:
+    Repository<Reporte>,
+  ){}
+  async create(createReporteDto: CreateReporteDto) {
+    try{
+      const Reporte = this.ReporteRepository.create
+      (createReporteDto);
+      await this.ReporteRepository.save(Reporte);
+    }catch (error){
+      console.log(error);
+      throw new InternalServerErrorException(`error al registrar el reporte`)
+    }
   }
 
-  findAll() {
-    return `This action returns all reporte`;
+  async findAll() {
+    return await this.ReporteRepository.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} reporte`;
+  async findOne(id: string) {
+    const Reporte = await this.ReporteRepository.findOneBy({id});
+    if(!Reporte){
+      throw new NotFoundException (`Reporte con id ${id} no existe`)
+    }
+    return Reporte;
   }
 
-  update(id: number, updateReporteDto: UpdateReporteDto) {
-    return `This action updates a #${id} reporte`;
+  async update(id: string, updateReporteDto: UpdateReporteDto) {
+    const Reporte = await this.ReporteRepository.preload({
+      id,
+      ...updateReporteDto
+    });
+    if(!Reporte){
+      throw new NotFoundException(`Reporte con id ${id} no existe`);
+    }
+    try{
+      await this.ReporteRepository.save(Reporte);
+      return Reporte
+    }catch(error){
+      console.log(error);
+      throw new InternalServerErrorException(`no se puede actualizar el reporte`)
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} reporte`;
+  async remove(id: string) {
+    const Reporte = await this.findOne(id);
+    await this.ReporteRepository.remove(Reporte);
+    return `Reporte eliminado exitosamente`;
   }
 }
