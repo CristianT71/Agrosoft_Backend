dae7a25fbc2cd0b2cbc3dd8aa545476f2fad1829
Juan Valencia <juandavidpv2006@gmail.com>
2026-05-20 19:35:07 -0500

    acabo de terminar de importar el entity y sus columnas

--- src/insumo/entities/insumo.entity.ts
+++ src/insumo/entities/insumo.entity.ts
@@ -1,1 +1,22 @@
-export class Insumo {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Decimal128 } from "typeorm/browser";
+
+@Entity()
+export class Insumo {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+    @Column()
+    categoria: string;
+    @Column()
+    unidad_medida: Decimal128;
+    @Column()
+    cantidad_disponible: Decimal128;
+    @Column()
+    precio_unitario: Decimal128;
+    @Column()
+    estado: string;
+
+}
