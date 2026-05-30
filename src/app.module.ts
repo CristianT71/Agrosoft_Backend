80afbeb679fededcfff6a3c77473e7eff888b9e5
Cesar <cesar.palmac@normalpitalito.edu.co>
2026-05-30 16:55:19 -0500

    creacion del modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { IncidenciaModule } from './incidencia/incidencia.module';
 import { EvidenciaModule } from './evidencia/evidencia.module';
 import { TipoIncidenciaModule } from './tipo_incidencia/tipo_incidencia.module';
 import { TipoIncidenciaModule } from './tipo_incidencia/tipo_incidencia.module';
+import { TipoIncidenciaModule } from './tipo_incidencia/tipo_incidencia.module';
 import { EvidenciaModule } from './evidencia/evidencia.module';
 
 @Module({
--- src/tipo_incidencia/tipo_incidencia.module.ts
+++ src/tipo_incidencia/tipo_incidencia.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TipoIncidenciaService } from './tipo_incidencia.service';
 import { TipoIncidenciaController } from './tipo_incidencia.controller';
-import { TipoIncidencia } from './tipo_incidencia.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TipoIncidencia])],
   controllers: [TipoIncidenciaController],
   providers: [TipoIncidenciaService],
-  exports: [TypeOrmModule],
 })
 export class TipoIncidenciaModule {}
