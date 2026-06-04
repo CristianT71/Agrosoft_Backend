6e2c87f3e60a3192070d5d890b014bbcb3373d32
alejandroVargas08 <manuelvargasnoriega@gmail.com>
2026-06-03 19:26:45 -0500

    Terminado Module

--- src/plan_manejo/plan_manejo.module.ts
+++ src/plan_manejo/plan_manejo.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PlanManejoService } from './plan_manejo.service';
 import { PlanManejoController } from './plan_manejo.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlanManejo } from './entities/plan_manejo.entity';
 
 @Module({
   controllers: [PlanManejoController],
   providers: [PlanManejoService],
+  imports:
+  [TypeOrmModule.forFeature([PlanManejo])]
 })
 export class PlanManejoModule {}
