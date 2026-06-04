248c4f4ddec4addd7599c55a84f3cddf7d30e3ff
alejandroVargas08 <manuelvargasnoriega@gmail.com>
2026-06-03 20:16:11 -0500

    Cuadre Module

--- src/insumo/insumo.module.ts
+++ src/insumo/insumo.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { InsumoService } from './insumo.service';
 import { InsumoController } from './insumo.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Insumo } from './entities/insumo.entity';
 
 @Module({
   controllers: [InsumoController],
   providers: [InsumoService],
+  imports:[
+    TypeOrmModule.forFeature([Insumo])
+  ]
 })
 export class InsumoModule {}
