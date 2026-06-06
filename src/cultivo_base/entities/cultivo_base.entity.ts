3790d2ccbc74f15a71eb6f0fa44c8026c54d1b1f
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-06-06 03:59:34 -0500

    feat(cultivo_base): crear entity de cultivo_base

--- src/cultivo_base/entities/cultivo_base.entity.ts
+++ src/cultivo_base/entities/cultivo_base.entity.ts
@@ -1,1 +1,20 @@
-export class CultivoBase {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class CultivoBase {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ type: 'varchar', length: 150 })
+    nombre: string;
+
+    @Column({ type: 'varchar', length: 50 })
+    tipo: string;
+
+    @Column({ type: 'text', nullable: true })
+    descripcion: string;
+
+    @Column({ type: 'varchar', length: 30, default: 'activo' })
+    estado: string;
+
+}
