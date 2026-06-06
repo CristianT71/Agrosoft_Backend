9c0ae9ffeac1c8d8197a4ffaa489ca77faa91832
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-06-06 04:22:21 -0500

    feat(cultivo_base): implementar crud y logica de negocio en el servicio

--- src/cultivo_base/cultivo_base.service.ts
+++ src/cultivo_base/cultivo_base.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateCultivoBaseDto } from './dto/create-cultivo_base.dto';
 import { UpdateCultivoBaseDto } from './dto/update-cultivo_base.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CultivoBase } from './entities/cultivo_base.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CultivoBaseService {
-  create(createCultivoBaseDto: CreateCultivoBaseDto) {
-    return 'This action adds a new cultivoBase';
+  constructor(
+    @InjectRepository(CultivoBase)
+      private readonly cultivoBaseRepository: Repository<CultivoBase>,
+  ){}
+
+  async create(createCultivoBaseDto: CreateCultivoBaseDto) {
+    try {
+      const cultivobase = this.cultivoBaseRepository.create(createCultivoBaseDto);
+      await this.cultivoBaseRepository.save(cultivobase);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Error al registrar el cultivo')
+    }
   }
 
-  findAll() {
-    return `This action returns all cultivoBase`;
+  async findAll() {
+    return this.cultivoBaseRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cultivoBase`;
+  async findOne(id: string) {
+    const cultivobase = await this.cultivoBaseRepository.findOneBy({ id });
+    if (!cultivobase) {
+      throw new NotFoundException(`Cultivo base con id ${id} no existe`)
+    }
+    return cultivobase;
   }
 
-  update(id: number, updateCultivoBaseDto: UpdateCultivoBaseDto) {
-    return `This action updates a #${id} cultivoBase`;
+  async update(id: string, updateCultivoBaseDto: UpdateCultivoBaseDto) {
+    const cultivobase = await this.cultivoBaseRepository.preload({
+      id,
+      ...updateCultivoBaseDto,
+    });
+    if (!cultivobase) {
+      throw new NotFoundException(`Cultivo base con id ${id} no existe`);
+    }
+    try {
+      await this.cultivoBaseRepository.save(cultivobase);
+      return cultivobase;
+    } catch (error){
+      console.log(error)
+      throw new InternalServerErrorException('Error al actualizar el cultivo')
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cultivoBase`;
+  async remove(id: string) {
+    const cultivobase = await this.findOne(id);
+    await this.cultivoBaseRepository.remove(cultivobase)
+    return 'Cultivo base eliminado con exito';
   }
 }
