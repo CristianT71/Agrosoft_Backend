68a3b1928ab7fd78831d5fcf12f1d9a37b1ed6af
jhennedy <chavarromurciajhennedy@gmail.com>
2026-06-10 19:43:09 +0530

    inyeccion de dependencia (modulo accion_ejecutada)

--- src/accion_ejecutada/accion_ejecutada.module.ts
+++ src/accion_ejecutada/accion_ejecutada.module.ts
@@ -3,13 +3,18 @@ import { AccionEjecutadaService } from './accion_ejecutada.service';
 import { AccionEjecutadaController } from './accion_ejecutada.controller';
 import { AccionEjecutada } from './entities/accion_ejecutada.entity';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { UsuarioModule } from '../usuario/usuario.module';
+import { CultivoRealModule } from '../cultivo_real/cultivo_real.module';
 
 @Module({
   controllers: [AccionEjecutadaController],
   providers: [AccionEjecutadaService],
+  exports: [AccionEjecutadaService],
 
 imports:[
   TypeOrmModule.forFeature([AccionEjecutada]),
+  UsuarioModule,
+  CultivoRealModule,
 ]
 })
 export class AccionEjecutadaModule {}
